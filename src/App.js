406e07daf22fb8fd8e068aa22ae8508cc5c79e65
LavinaCaco <cacotugas@gmail.com.com>
2025-05-13 13:59:46 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './view/home';
-import Mobil from './view/mobil';
+
 import ScrollToTop from './components/ScrollToTop'; 
 
 export default function App() {
@@ -10,7 +10,7 @@ export default function App() {
       <ScrollToTop /> 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mobil" element={<Mobil />} />
+
       </Routes>
     </>
   );
